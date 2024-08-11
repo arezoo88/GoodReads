4b4f2916a7c4f059bcd5fb4c9e3ec86c418aa9b9
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 10:58:02 +0330

    add url for getting book detail

--- source/apps/book/api/v1/urls.py
+++ source/apps/book/api/v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from apps.book.api.v1.views import BookViewSet
+from apps.book.api.v1.views import BookViewSet, BookDetailViewSet
 
 app_name = 'book.api.v1'
 
 
 urlpatterns = [
-    path('books/', BookViewSet.as_view({'get': 'list'}), name='book'),
+    path('books/', BookViewSet.as_view({'get': 'list'}), name='book-list'),
+    path('books/<int:pk>/',
+         BookDetailViewSet.as_view({'get': 'retrieve'}), name='book-detail'),
 
 ]
