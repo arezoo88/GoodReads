4b1108ded588868b96cd72b22a9f9d6922a5f6fe
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 09:26:22 +0330

    include urls of account app

--- source/base/urls.py
+++ source/base/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.book.api.v1.urls', namespace='book.v1')),]
+    path('api/v1/', include('apps.book.api.v1.urls', namespace='book.v1')),
+    path('api/v1/', include('apps.account.api.v1.urls', namespace='account.v1')),
+
+]
