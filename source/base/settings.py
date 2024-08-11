586629baee25520953bcc2ada95fd4c09a6edadf
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 09:20:12 +0330

    add rest_framework and book app to installed_apps

--- source/base/settings.py
+++ source/base/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Parties
+    'rest_framework',
+    'rest_framework_simplejwt',
+    # Local Apps
+    'apps.book'
 ]
 
 MIDDLEWARE = [
