efe6196dc21a631b33c32d3a6fd7fafd4329dc58
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 10:58:19 +0330

    create api view for getting book detail

--- source/apps/book/api/v1/views.py
+++ source/apps/book/api/v1/views.py
@@ -2,9 +2,20 @@ from rest_framework.viewsets import ModelViewSet
 from apps.book.api.v1.serializers import BookSerializer
 from apps.book.models import Book
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = BookSerializer
+
+
+class BookDetailViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = BookSerializer
+
+    def get_object(self):
+        book_obj = get_object_or_404(Book, pk=self.kwargs['pk'])
+        return book_obj
