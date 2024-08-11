261ba0c5cb96c0d1f3d38157cae4c75d69d3abdb
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 17:41:38 +0330

    create post url for  ratingcomment api

--- source/apps/book/api/v1/urls.py
+++ source/apps/book/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from apps.book.api.v1.views import BookViewSet, BookDetailViewSet
+from apps.book.api.v1.views import BookViewSet, BookDetailViewSet, RatingCommentViewSet
 
 app_name = 'book.api.v1'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('books/', BookViewSet.as_view({'get': 'list'}), name='book-list'),
     path('books/<int:pk>/',
          BookDetailViewSet.as_view({'get': 'retrieve'}), name='book-detail'),
+    path('rate-comment/',
+         RatingCommentViewSet.as_view({'post': 'create'}), name='rate-and-comment'),
 
 ]
