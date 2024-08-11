7805ad01d2ea269f4bb9880c218e6e16da67513c
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 10:40:03 +0330

    create url for displaying  books list

--- source/apps/book/api/v1/urls.py
+++ source/apps/book/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from apps.book.api.v1.views import BookViewSet
 
 app_name = 'book.api.v1'
 
 
 urlpatterns = [
-
+    path('books/', BookViewSet.as_view({'get': 'list'}), name='book'),
 
 ]
