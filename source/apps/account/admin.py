a0ff75e5b04f5e3a2e1f558d306dbed1c5c207f5
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 10:37:45 +0330

    customize admin

--- source/apps/account/admin.py
+++ source/apps/account/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_superuser','is_staff')
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
--- source/apps/book/admin.py
+++ source/apps/book/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from apps.book.models import Book, BookMark
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'summary','created_at')
+    list_filter = ('title',)
+
+
+class BookMarkAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user')
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookMark, BookMarkAdmin)
