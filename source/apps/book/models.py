f8e3270bd3495f89c98bcd56f4c41d92d98df755
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-11 09:13:12 +0330

    create book ,rateandcomment,bookmark models

---
+++ source/apps/book/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name=_('Created_at'),
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_('Updated_at'),
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Book(BaseModel):
+    title = models.CharField(
+        max_length=200,
+        verbose_name=_('Title'),
+    )
+    summary = models.TextField(
+        verbose_name=_('Summary')
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = _('Book')
+        verbose_name_plural = _('Books')
+
+    def __str__(self):
+        return self.title
+
+
+class RateAndComment(BaseModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('User'),
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        verbose_name=_('Book'),
+    )
+    rating = models.IntegerField(
+        choices=[(i, i) for i in range(1, 6)],
+        verbose_name=_('Rating'),
+        null=True,
+        blank=True
+    )
+    comment = models.TextField(
+        verbose_name=_('Summary'),
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        unique_together = ('user', 'book')
+        ordering = ['-created_at']
+        verbose_name = _('RateAndComment')
+        verbose_name_plural = _('RatesAndComments')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
+
+
+class BookMark(BaseModel):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('User'),
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        verbose_name=_('Book'),
+    )
+
+    class Meta:
+        unique_together = ('user', 'book')
+        ordering = ['-created_at']
+        verbose_name = _('BookMark')
+        verbose_name_plural = _('BookMarks')
