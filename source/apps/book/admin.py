ed87125f7f20a6bf74c176f8daf366d1fc72a697
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-12 11:27:40 +0330

    customize admin panel and add ratingcomment model in dashboard

--- source/apps/book/admin.py
+++ source/apps/book/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from apps.book.models import Book, BookMark
+from apps.book.models import Book, BookMark, RatingComment
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'summary','created_at')
+    list_display = ('title', 'summary', 'created_at', 'bookmark_count')
     list_filter = ('title',)
+
+
+class RatingCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'rating', 'comment')
+    list_filter = ('rating',)
 
 
 class BookMarkAdmin(admin.ModelAdmin):
@@ -12,4 +17,5 @@ class BookMarkAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(RatingComment, RatingCommentAdmin)
 admin.site.register(BookMark, BookMarkAdmin)
