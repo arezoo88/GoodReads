c9c114e897ef6a4ee80fef4b15bb1544e2bbed3c
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-12 11:26:42 +0330

    remove extra import

--- source/apps/account/api/v1/views.py
+++ source/apps/account/api/v1/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from apps.account.api.v1.serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 
 User = get_user_model()
